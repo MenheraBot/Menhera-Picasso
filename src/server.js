c0a5cb968f49664565abff7068b30f96356c3b30
Luxanna <CommandPlayermine@gmail.com>
2021-09-23 14:19:31 -0300

    feat: Add ping pong system

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const { startAllNeeded } = require('./utils/StartBulders')
 const express = require("express")
 const cors = require('cors')
+const url = require('url')
 const app = express()
 const routes = require('./routes')
 const { Server } = require('ws')
@@ -14,20 +15,44 @@ const initServer = async () => {
   app.use(cors())
   app.use(express.json())
 
-  app.get('/ping', (_, res) => res.status(200).json({ message: 'Heyyy, im up', uptime: Date.now() - startTime }))
+  const httpServer = app.listen(2080, () => {
+    console.log('[SERVER] Menhera Picasso started at port 2080')
+  });
+
+  const ws = new Server({ server: httpServer });
+  ws.on('connection', (socket, req) => {
+    socket.id = url.parse(req.url, true).query.id ?? 0
+    socket.isAlive = true;
+    socket.uptime = Date.now()
+
+    socket.on('message', (rawRequest) => WebSocketController(socket, rawRequest));
+
+    socket.on('pong', (ms) => {
+      socket.isAlive = true;
+      socket.responseTime = Date.now() - ms.toString();
+    })
+  })
+
+  app.get('/ping', (_, res) => {
+    const clients = []
+    ws.clients.forEach(a => clients.push({ id: a.id, ping: a.responseTime, uptime: Date.now() - a.uptime }))
+    res.status(200).json({ http: { uptime: Date.now() - startTime }, ws: [...ws.clients.values()].map(a => ({ id: Number(a.id), ping: a.responseTime, uptime: Date.now() - a.uptime })) })
+  })
+
+  setInterval(() => {
+    ws.clients.forEach((skt) => {
+      if (skt.isAlive === false) return skt.terminate()
+
+      skt.isAlive = false;
+      skt.ping(Date.now())
+    })
+  }, 15000)
 
   app.use(routes, isAuthorized)
 
   app.use((_req, res) => {
     res.status(404).json({ message: 'Welp, there is nothing for you right here' });
   })
-
-  const httpServer = app.listen(2080, () => {
-    console.log('[SERVER] Menhera Picasso started at port 2080')
-  });
-
-  const ws = new Server({ server: httpServer });
-  ws.on('connection', WebSocketController)
 }
 
 initServer();
