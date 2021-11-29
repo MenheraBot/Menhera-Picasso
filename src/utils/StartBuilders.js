9b9d8f515e7af337e2ce5d95a23d2f730876e8a5
Luxanna <CommandPlayermine@gmail.com>
2021-11-29 15:31:44 -0300

    feat: Add christmas foint

--- src/utils/StartBuilders.js
+++ src/utils/StartBuilders.js
@@ -6,6 +6,7 @@ const { Start: StartBadges } = require('./ProfileUtils')
 
 const startAllNeeded = async () => {
   registerFont(join(__dirname, '..', 'assets', 'fonts', 'impact.ttf'), { family: 'Impact' })
+  registerFont(join(__dirname, '..', 'assets', 'fonts', 'candy.ttf'), { family: 'Candy' })
   StartBadges()
   canvas.start()
   CardsStarter.Start()
